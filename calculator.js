089fe3158e23fac1be99f5674ce12252d662b8b5
Eric Tsiliacos <erictsiliacos@gmail.com>
2015-01-17 14:46:06 -0800

    switched from for loop to .each using the underscore library

--- calculator.js
+++ calculator.js
@@ -1,39 +1,39 @@
 window.onload = function(){
 
-  var buttons = document.getElementsByClassName('button');
+  var numberElements = document.getElementsByClassName('number');
+  var operatorElements = document.getElementsByClassName('operator');
+  var clearElement = document.getElementById('clear');
+  var enterElement = document.getElementById('enter');
   var calcDisplay = document.getElementById('operator_display');
+
   var totalUserInputs = [];
   var currentOperator = null;
 
   resetUI(calcDisplay);
 
-  for (var i = 0; i < buttons.length; i++) {
-    var buttonText = buttons[i].innerHTML
-    if (buttonText === "CLR" ) {
-      buttons[i].addEventListener("click", function() {
-        resetUI(calcDisplay);
-      });
+  _.each(numberElements, function(numberElement) {
+    numberElement.addEventListener("click", function() {
+      value = processNumber(totalUserInputs, this.innerHTML, currentOperator);
+      calcDisplay.innerHTML = value;
+      totalUserInputs.push(value);
+    });
+  });
 
-    } else if (buttonText === "ENT") {
-      buttons[i].addEventListener("click", function() {
-        output = processEnter(totalUserInputs, currentOperator)
-        calcDisplay.innerHTML = output
-        totalUserInputs.push(value);
-      });
+  _.each(operatorElements, function(operatorElement) {
+    operatorElement.addEventListener("click", function(){
+      currentOperator = this.innerHTML;
+    });
+  });
 
-    } else if (buttonText === "+" || buttonText === "-" || buttonText === "*" || buttonText === "/") {
-      buttons[i].addEventListener("click", function(){
-        currentOperator = this.innerHTML;
-      });
+  clearElement.addEventListener("click", function() {
+    resetUI(calcDisplay);
+  });
 
-    } else {
-      buttons[i].addEventListener("click", function() {
-        value = processNumber(totalUserInputs, this.innerHTML, currentOperator);
-        calcDisplay.innerHTML = value;
-        totalUserInputs.push(value);
-      });
-    }
-  }
+  enterElement.addEventListener("click", function() {
+    output = processEnter(totalUserInputs, currentOperator)
+    calcDisplay.innerHTML = output
+    totalUserInputs.push(value);
+  });
 }
 
 var resetUI = function(calcDisplay) {
