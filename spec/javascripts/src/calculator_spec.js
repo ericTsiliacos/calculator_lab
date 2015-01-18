cdb2273b104ff3245a65d5de3421c77607eccc7b
Eric Tsiliacos <erictsiliacos@gmail.com>
2015-01-17 17:04:18 -0800

    move calculator spec to spec/src

---
+++ spec/javascripts/src/calculator_spec.js
@@ -0,0 +1,51 @@
+describe('calculator', function() {
+  describe('processEnter', function() {
+    describe('when the user has inputed two inputs', function() {
+      it('evaluates the last two inputs with the current operator', function() {
+        expect((new Calculator).processEnter(["1", "2"], "+")).toEqual(3);
+      });
+    });
+
+    describe('when the user has inputed one input', function() {
+      it('evaluates the last input to itself with the current operator', function() {
+        expect(Calculator.processEnter(["2"], "+")).toEqual(4);
+      });
+    });
+  });
+
+  describe('processNumber', function() {
+    describe("when the user has not inputed anything and has not selected an operator", function() {
+      it('returns the inputed number as the number to display', function() {
+        var lastInputs = [];
+        var currentInput = "1";
+        var currentOperator = null;
+
+        var result = Calculator.processNumber(lastInputs, currentInput, currentOperator);
+        expect(result).toEqual("1");
+      });
+    });
+
+    describe("when the user has already inputed numbers and has not selected an opereator", function() {
+      it('concatenates the last user input with the current input', function() {
+        var lastInputs = ["2", "1"];
+        var currentInput = "1";
+        var currentOperator = null;
+
+        var result = Calculator.processNumber(lastInputs, currentInput, currentOperator)
+        expect(result).toEqual("11");
+      });
+    });
+
+    describe("when the user has inputed a single number and has selected an operator", function() {
+      it('returns the inputed number as the number to display', function() {
+        var lastInputs = ["1"];
+        var currentInput = "1";
+        var currentOperator = "+";
+
+        var result = Calculator.processNumber(lastInputs, currentInput, currentOperator);
+        expect(result).toEqual("1");
+      });
+    });
+  });
+});
+
